f040476f06dc0fd2f382642328a03047b83bcb2e
noFrostoo <37929008+noFrostoo@users.noreply.github.com>
2020-03-30 22:37:32 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 # main.py
-
+from typing import Dict
 from fastapi import FastAPI
-
+from pydantic import BaseModel
 app = FastAPI()
+
+app.counter = 0
 
 
 @app.get("/")
@@ -14,18 +16,58 @@ def hello_world():
 def hello_name(name: str):
     return {"message": f"Hello {name}"}
 
+
 @app.get("/method")
-def hello_world():
+def method_get():
     return {"method": "GET"}
 
+
 @app.post("/method")
-def hello_world():
+def method_post():
     return {"method": "POST"}
 
+
 @app.put("/method")
-def hello_world():
+def method_put():
     return {"method": "PUT"}
 
+
 @app.delete("/method")
-def hello_world():
+def method_delete():
     return {"method": "DELETE"}
+
+
+@app.get('/counter')
+def counter():
+    app.counter += 1
+    return str(app.counter)
+
+
+class GiveMeSomethingRq(BaseModel):
+    first_key: str
+
+
+class GiveMeName(BaseModel):
+    name: str
+    surname: str
+
+
+class ResponeName(BaseModel):
+    app.counter: int
+    patient: Dict
+
+
+class GiveMeSomethingResp(BaseModel):
+    received: Dict
+    constant_data: str = "python jest super"
+
+
+@app.post("/dej/mi/coś", response_model=GiveMeSomethingResp)
+def receive_something(rq: GiveMeSomethingRq):
+    return GiveMeSomethingResp(received=rq.dict())
+
+
+@app.post("/patient", response_model=GiveMeName)
+def receive_name(rq: GiveMeName):
+    app.counter += 1
+    return ResponeName(patient=rq.dict())
