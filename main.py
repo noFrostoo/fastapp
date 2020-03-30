c9bf338039e231faaaad27bc87950d9a95d7357e
noFrostoo <37929008+noFrostoo@users.noreply.github.com>
2020-03-30 21:37:01 +0200

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = FastAPI()
 
 @app.get("/")
 def hello_world():
-    return {"message": "Hello World"}
+    return {"message": "Hello World during the coronavirus pandemic!"}
 
 
 @app.get("/hello/{name}")
