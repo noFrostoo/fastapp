03a1197e1191533e03d19a744b644341a87b6869
noFrostoo <37929008+noFrostoo@users.noreply.github.com>
2020-03-30 21:50:15 +0200

    Update main.py

--- main.py
+++ main.py
@@ -13,3 +13,19 @@ def hello_world():
 @app.get("/hello/{name}")
 def hello_name(name: str):
     return {"message": f"Hello {name}"}
+
+@app.get("/method")
+def hello_world():
+    return {"method": "GET"}
+
+@app.post("/method")
+def hello_world():
+    return {"method": "POST"}
+
+@app.put("/method")
+def hello_world():
+    return {"method": "PUT"}
+
+@app.delete("/method")
+def hello_world():
+    return {"method": "DELETE"}
