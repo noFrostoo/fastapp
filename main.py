0de081b75baea2437a8ed5ebd3a3ba27acd8289f
noFrostoo <37929008+noFrostoo@users.noreply.github.com>
2020-04-26 17:23:17 +0200

    Update main.py

--- main.py
+++ main.py
@@ -127,7 +127,7 @@ def logiing_in(response: Response, username: str = Depends(get_current_username)
     session_token = sha256(bytes(f"{username}{app.secret_key}", encoding="utf8")).hexdigest()
     response = RedirectResponse("/Welcome")
     response.set_cookie(key="session_token", value=session_token)
-    response.status_code = 200
+    response.status_code = 301
     tookens.append(session_token)
     return response
 
