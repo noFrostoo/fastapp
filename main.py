0ae7e4159969bc9e7748c578c8900be5135d16b0
noFrostoo <37929008+noFrostoo@users.noreply.github.com>
2020-05-03 12:54:14 +0200

    Update main.py

--- main.py
+++ main.py
@@ -205,7 +205,7 @@ def logout():
 async def root(page: int = Query(0), per_page: int = Query(10)):
     tracks = app.db_connection.execute("SELECT * FROM tracks").fetchall()
     to_return = []
-    for t in tracks[:10]:
+    for t in tracks[page:per_page+1]:
         to_return.append({
             "TrackId": t[0],
             "Name": t[1],
